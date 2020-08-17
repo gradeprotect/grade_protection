7263c74a914540f5e5f18488c8f442755c3d2cac
gradeprotect <1667608810@qq.com>
2020-08-17 13:01:31 +0800

    docs:add application.yml

---
+++ src/main/java/com/das/MainApplication.java
@@ -0,0 +1,15 @@
+package com.das;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * @author 许文滨
+ */
+public class MainApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
